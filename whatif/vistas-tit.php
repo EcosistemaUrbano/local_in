ac721046a66952ac360480b8cf16346b7a86616c
skotperez <voraginebramante@gmail.com>
2014-10-01 18:19:10 +0200

    fix some errors in list title output

--- whatif/vistas-tit.php
+++ whatif/vistas-tit.php
@@ -1,11 +1,18 @@
 <?php
-	$tags_tit = get_the_title();
+	if ( is_tax() ) {
+		$term =	$wp_query->queried_object->name;
+		$tags_tit = sprintf( __('Mensajes con la etiqueta %s','whatif'),$term );
+	} else {
+		$tags_tit = get_the_title();
+	}
 	$tags_id = $post->ID;
-	if ( post_custom("Page Small Icon") ) { $pag_img = get_post_meta($post->ID, "Page Small Icon", $single = true); }
-	
+	if ( post_custom("Page Small Icon") ) {
+		$pag_img = get_post_meta($post->ID, "Page Small Icon", $single = true);
+		$pag_img_out = "<img src='" .WHATIF_BLOGTHEME. "/images/$pag_img' title='$tags_tit'  alt='$tags_tit' />";
+	} else { $pag_img_out = ""; }
 	$tit_out = "
 	<div class='tit-peq'>
-		<img src='" .WHATIF_BLOGTHEME. "/images/$pag_img' title='$tags_tit'  alt='$tags_tit' />
+		$pag_img_out
 		<h2>$tags_tit</h2>
 	</div>
 	";
