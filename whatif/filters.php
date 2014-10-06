1429d4794d85055033b76056c1c5f3e76c694070
skotperez <voraginebramante@gmail.com>
2014-10-06 16:34:36 +0200

    activation and deactivation options in filters

--- whatif/filters.php
+++ whatif/filters.php
@@ -1,27 +1,48 @@
 <?php
 	// the categories filter
-	$perma = get_permalink();
-	if ( $pn == 'positivo' ) { $pl_class = " class='active'"; $mn_class = ""; }
-	elseif ( $pn == 'negativo' ) { $mn_class = " class='active'"; $pl_class = ""; }
-	else { $pl_class = $mn_class = ""; }
+	$pn_options = array(
+		array(
+			'name' => 'positivo',
+			'title' => __('Mensajes positivos','whatif'),
+			'img' => 'pl-mini.png'
+		),
+		array(
+			'name' => 'negativo',
+			'title' => __('Mensajes negativos','whatif'),
+			'img' => 'mn-mini.png'
+		)
+	);
+
+	$perma = $perma_pn = $perma_filtro = get_permalink(). "?";
+
 	$filter_out = "<ul class='filter-cats'>";
 	$filter_out .= "
 		<li id='tax-reset' class='filter-cat'>
 			<a title='" . __('Restablecer','whatif') . "' href='" .$perma. "'><img src='" .WHATIF_BLOGTHEME. "/images/reset.png' style='width:30px;' alt='" . __('Restablecer','whatif') . "' /></a>
 			<div class='filter-tit'><a href=''></a></div>
 		</li>
-			
-		<li id='tax-positivo' class='filter-cat'>
-			<a" .$pl_class. " title='" . __('Mensajes positivos','whatif') . "' href='" .$perma. "?pn=positivo'><img src='" .WHATIF_BLOGTHEME. "/images/pl-mini.png' alt='" . __('Mensajes positivos','whatif') . "' /></a>
-		</li>
-		<li id='tax-negativo' class='filter-cat'>
-			<a" .$mn_class. " title='" . __('Mensajes negativos','whatif') . "' href='" .$perma. "?pn=negativo'><img src='" .WHATIF_BLOGTHEME. "/images/mn-mini.png' alt='" . __('Mensajes negativos','whatif') . "' /></a>
-		</li>
 	";
+
+	foreach ( $pn_options as $option ) {
+		$perma_pn = $perma;
+		if ( $option['name'] == $pn ) { $pn_class = " class='active'"; $perma_pn .= ""; }
+		else { $perma_pn .= "pn=" .$option['name']. "&"; $pn_class = ""; }
+		if ( $filtro != '' ) { $perma_pn .= "filtro=" .$filtro. "&"; }
+		$perma_pn = substr($perma_pn, 0, -1);
+
+		$filter_out .= "<li id='tax-" .$option['name']. "' class='filter-cat'>
+			<a" .$pn_class. " title='" .$option['title']. "' href='" .$perma_pn. "'><img src='" .WHATIF_BLOGTHEME. "/images/" .$option['img']. "' alt='" .$option['title']. "' /></a></li>
+		";
+	}
+
 	foreach ( get_categories() as $categ ) {
 		$cat_id = $categ->term_id;
-		if ( $cat_id == $filtro ) { $filter_class = " class='active'"; }
-		else { $filter_class = ""; }
+		$perma_filtro = $perma;
+		if ( $cat_id == $filtro ) { $filter_class = " class='active'"; $perma_filtro .= ""; }
+		else { $perma_filtro .= "filtro=" .$cat_id. "&"; $filter_class = ""; }
+		if ( $pn != '' ) { $perma_filtro .= "pn=" .$pn. "&"; }
+		$perma_filtro = substr($perma_filtro, 0, -1);
+
 		$cat_perma = get_category_link($categ->term_id);
 		$cat_meta = get_option( "taxonomy_$cat_id" );
 		$cat_img = $cat_meta['image'];
@@ -32,15 +53,12 @@
 			$categImg = "<img src='" .$cat_img. "' alt='" .$categ->name. "' />";
 		} else { $categImg = $categ->name; }
 		
-		$identificador = $categ->slug;
-		$identificador = str_replace("-","",$identificador);
-		
 		$filter_out .= "
 			<li id='$categ->slug' class='filter-cat'>
-			<div>
-			<a" .$filter_class. " href='" .$perma. "?filtro=$cat_id&pn=$pn2'>$categImg</a>
-			</div>
-			<div class='filter-tit'>$categ->category_count</div>
+				<div>
+				<a" .$filter_class. " href='" .$perma_filtro. "'>$categImg</a>
+				</div>
+				<div class='filter-tit'>$categ->category_count</div>
 			</li>
 		";
 		
@@ -48,5 +66,4 @@
 	$filter_out .= "</ul><!-- end class mess-cats -->";
 
 	echo $filter_out;
-	
 ?>
