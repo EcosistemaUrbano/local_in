1bcc101d35cffe397d6b753cad52dc3654bcfdf5
skotperez <voraginebramante@gmail.com>
2014-10-16 11:14:52 +0200

    404 error template

---
+++ whatif/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="tit-peq-page"><h2><?php _e('Error 404: Page not found','whatif'); ?></h2></div>
+
+<div class="unique-pages">
+	<div class="unique-text bg-error">
+		<p><?php _e("We didn't find the content you have requested. Check the URL or start again at home page.","whatif"); ?></p>
+	</div>
+</div>
+
+<?php get_footer(); ?>
+
